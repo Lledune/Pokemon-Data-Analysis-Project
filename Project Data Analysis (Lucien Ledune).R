94f7e3fcdbd6b6cc831a22cc63c0cc7de58f0bcd
dfghdf89 <lucien.ledune@student.uclouvain.be>
2017-11-24 14:12:24 +0100

    Introduction + présentation des données

--- Project Data Analysis (Lucien Ledune).R
+++ Project Data Analysis (Lucien Ledune).R
@@ -7,12 +7,20 @@ library(FactoMineR)
 library(MASS)
 
 #Preparing data
-Data = read.csv("https://raw.githubusercontent.com/dfghdf89/Pokemon-Data-Analysis-Project/master/Pokemon.csv")
+Data = read.csv("https://raw.githubusercontent.com/dfghdf89/Pokemon-Data-Analysis-Project/master/Pokemon.csv", sep = ";")
 
-#Names as column 
-names = Data$Name
-row.names(Data) = names
+#Keeping gen1 only
+Data = subset(Data, Data$Generation == 1)
 Data = as.matrix.data.frame(Data)
+
+#Analyse descriptive
+
+
+
+
+
+
+
 
 #Delete name and Type2 cols
 Data = Data[,c(1, 3, 5:13)]
@@ -24,7 +32,9 @@ DataPCA = apply(DataPCA, 2, as.numeric)
 #PCA Analysis of the Data.
 res.pca = PCA(DataPCA, ncp = 3, scale.unit = TRUE )
 
-#Matrix for lda
 
-#LDA, legendar or not ?
-lda(Data)
+
+
+
+
+
