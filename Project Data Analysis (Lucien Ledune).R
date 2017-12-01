57fde6e5cacb95c2a98fcfddb6db7418ad608a8a
dfghdf89 <lucien.ledune@student.uclouvain.be>
2017-12-01 04:47:38 +0100

    First version

--- Project Data Analysis (Lucien Ledune).R
+++ Project Data Analysis (Lucien Ledune).R
@@ -11,26 +11,23 @@ Data = read.csv("https://raw.githubusercontent.com/dfghdf89/Pokemon-Data-Analysi
 
 #Keeping gen1 only
 Data = subset(Data, Data$Generation == 1)
-Data = as.matrix.data.frame(Data)
 
 #Analyse descriptive
 
-
-
-
-
-
-
+#Preparing subsets 
+subsets = split(Data, Data$Type.1, drop=TRUE)
 
 #Delete name and Type2 cols
 Data = Data[,c(1, 3, 5:13)]
-DataPCA = Data[, c(3:9)]
+DataPCA = Data[, c(4:9)]
 
 #Converting Data into numeric values (Correcting import problem from csv)
 DataPCA = apply(DataPCA, 2, as.numeric)
 
 #PCA Analysis of the Data.
-res.pca = PCA(DataPCA, ncp = 3, scale.unit = TRUE )
+plot.HCPC(reS.hcpc, tree.barplot  = T, choice = "bar")
+
+
 
 
 
